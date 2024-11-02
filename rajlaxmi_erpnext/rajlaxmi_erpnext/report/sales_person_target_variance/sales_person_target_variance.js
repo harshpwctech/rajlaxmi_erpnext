352e7e8a1eea850b391cf22cdff578294ae254fc
Harsh <apple@apples-MacBook-Air.local>
2024-11-02 15:23:17 +0530

    Modified the report as per the new format

--- rajlaxmi_erpnext/rajlaxmi_erpnext/report/sales_person_target_variance/sales_person_target_variance.js
+++ rajlaxmi_erpnext/rajlaxmi_erpnext/report/sales_person_target_variance/sales_person_target_variance.js
@@ -1,38 +1,85 @@
 frappe.query_reports["Sales Person Target Variance"] = {
-	filters: [
+	"filters": [
 		{
-			fieldname: "company",
-			label: __("Company"),
-			fieldtype: "Link",
-			options: "Company",
-			default: frappe.defaults.get_user_default("Company"),
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
 		},
 		{
-			fieldname: "fiscal_year",
-			label: __("Fiscal Year"),
-			fieldtype: "Link",
-			options: "Fiscal Year",
-			default: erpnext.utils.get_fiscal_year(frappe.datetime.get_today()),
-		},
-		{
-			fieldname: "period",
-			label: __("Period"),
-			fieldtype: "Select",
-			options: [
-				{ value: "Monthly", label: __("Monthly") },
-				{ value: "Quarterly", label: __("Quarterly") },
-				{ value: "Half-Yearly", label: __("Half-Yearly") },
-				{ value: "Yearly", label: __("Yearly") },
+			"fieldname": "period",
+			"label": __("Period"),
+			"fieldtype": "Select",
+			"options": [
+				{ "value": "MTD", "label": __("Month to Date") },
+				{ "value": "Month", "label": __("Month") },
+				{ "value": "Fiscal Year", "label": __("Fiscal Year") }
 			],
-			default: "Monthly",
+			"default": "Fiscal Year",
+            "reqd": 1
 		},
         {
-            fieldname: "sales_person",
-            label: __("Sales Person"),
-			fieldtype: "Link",
-			options: "Sales Person"
+			"fieldname": "date",
+			"label": __("Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+            "depends_on": 'eval:doc.period === "MTD"'
+		},
+        {
+			"fieldname": "fiscal_year",
+			"label": __("Fiscal Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": erpnext.utils.get_fiscal_year(frappe.datetime.get_today()),
+            "depends_on": 'eval:doc.period === "Fiscal Year"'
+		},
+        {
+			"fieldname": "month",
+			"label": __("Month"),
+			"fieldtype": "Select",
+			"options": [
+				{ "value": 1, "label": __("Jan") },
+				{ "value": 2, "label": __("Feb") },
+				{ "value": 3, "label": __("Mar") },
+                { "value": 4, "label": __("Apr") },
+                { "value": 5, "label": __("May") },
+                { "value": 6, "label": __("Jun") },
+                { "value": 7, "label": __("Jul") },
+                { "value": 8, "label": __("Aug") },
+                { "value": 9, "label": __("Sep") },
+                { "value": 10, "label": __("Oct") },
+                { "value": 11, "label": __("Nov") },
+                { "value": 12, "label": __("Dec") },
+			],
+            "depends_on": 'eval:doc.period === "Month"',
+            "default": frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth() + 1
+		},
+        {
+			"fieldname":"year",
+			"label": __("Year"),
+			"fieldtype": "Select",
+            "depends_on": 'eval:doc.period === "Month"',
+		},
+        {
+            "fieldname": "sales_person",
+            "label": __("Sales Person"),
+			"fieldtype": "Link",
+			"options": "Sales Person"
         }
 	],
+    onload: function() {
+		return  frappe.call({
+			method: "rajlaxmi_erpnext.rajlaxmi_erpnext.report.sales_person_target_variance.sales_person_target_variance.get_attendance_years",
+			callback: function(r) {
+				var year_filter = frappe.query_report.get_filter('year');
+				year_filter.df.options = r.message;
+				year_filter.df.default = r.message.split("\n")[0];
+				year_filter.refresh();
+				year_filter.set_input(year_filter.df.default);
+			}
+		});
+	},
 	formatter: function (value, row, column, data, default_formatter) {
 		value = default_formatter(value, row, column, data);
 
--- rajlaxmi_erpnext/rajlaxmi_erpnext/report/sales_person_target_variance/sales_person_target_variance.py
+++ rajlaxmi_erpnext/rajlaxmi_erpnext/report/sales_person_target_variance/sales_person_target_variance.py
@@ -1,273 +1,306 @@
 import frappe
 from frappe import _
-
+from frappe.query_builder.functions import Count, Extract, Sum
 from erpnext.accounts.doctype.monthly_distribution.monthly_distribution import (
-	get_periodwise_distribution_data,
+    get_periodwise_distribution_data,
 )
 from erpnext.accounts.report.financial_statements import get_period_list
-from erpnext.accounts.utils import get_fiscal_year
-
+from erpnext.accounts.utils import get_fiscal_year, getdate, cint, cstr
+from frappe.utils.data import get_first_day, get_last_day, date_diff
 
 def execute(filters=None):
-	return get_data_column(filters, "Sales Person")
+    return get_data_column(filters, "Sales Person")
 
 def get_data_column(filters, partner_doctype):
-	data = []
-	period_list = get_period_list(
-		filters.fiscal_year,
-		filters.fiscal_year,
-		"",
-		"",
-		"Fiscal Year",
-		filters.period,
-		company=filters.company,
-	)
-	rows = get_data(filters, period_list, partner_doctype)
-	columns = get_columns(filters, period_list, partner_doctype)
+    data = []
+    columns = get_columns(partner_doctype)
+    rows = get_data(filters, partner_doctype)
+    if not rows:
+        return columns, data
 
-	if not rows:
-		return columns, data
+    for key, value in rows.items():
+        value.update({"team": frappe.db.get_value(partner_doctype, {"name": key}, fieldname="department")})
+        value.update({"team_lead": frappe.db.get_value(partner_doctype, {"name": key}, fieldname="parent_sales_person")})
+        value.update({frappe.scrub(partner_doctype): key[0]})
 
-	for key, value in rows.items():
-		value.update({frappe.scrub(partner_doctype): key[0], "item_group": key[1]})
+        data.append(value)
 
-		data.append(value)
+    return columns, data
 
-	return columns, data
+def get_columns(partner_doctype):
+    fieldtype, options = "Currency", "currency"
 
+    columns = [
+        {
+            "fieldname": "team",
+            "label": _("Team"),
+            "fieldtype": "Link",
+            "options": "Department",
+            "width": 150,
+        },
+        {
+            "fieldname": "team_lead",
+            "label": _("Team Lead"),
+            "fieldtype": "Link",
+            "options": partner_doctype,
+            "width": 150,
+        },
+        {
+            "fieldname": frappe.scrub(partner_doctype),
+            "label": _(partner_doctype),
+            "fieldtype": "Link",
+            "options": partner_doctype,
+            "width": 150,
+        },
+        {
+            "fieldname": "experience",
+            "label": _("Total Experience"),
+            "fieldtype": "Float",
+            "default": 0.00,
+            "width": 100,
+        },
+        {
+            "fieldname": "customer_acquisition",
+            "label": _("Customer Acquisition"),
+            "fieldtype": "Int",
+            "default": 0,
+            "width": 150,
+        },
+        {
+            "fieldname": "total_target",
+            "label": _("Total Target"),
+            "fieldtype": fieldtype,
+            "options": options,
+            "width": 150,
+        },
+        {
+            "fieldname": "total_achieved",
+            "label": _("Total Achieved"),
+            "fieldtype": fieldtype,
+            "options": options,
+            "width": 150,
+        },
+        {
+            "fieldname": "per_achieved",
+            "label": _("% Achieved vs Target"),
+            "fieldtype": "Percent",
+            "width": 150,
+        },
+        {
+            "fieldname": "total_variance",
+            "label": _("Total Variance"),
+            "fieldtype": fieldtype,
+            "options": options,
+            "width": 150,
+        },
+        {
+            "fieldname": "per_day",
+            "label": _("Per Day"),
+            "fieldtype": fieldtype,
+            "options": options,
+            "width": 150,
+            "default": 0.00,
+        },
+        {
+            "fieldname": "salary",
+            "label": _("Salary"),
+            "fieldtype": fieldtype,
+            "options": options,
+            "width": 150,
+        },
+        {
+            "fieldname": "per_salary",
+            "label": _("% Achieved vs Salary"),
+            "fieldtype": "Percent",
+            "width": 150,
+        },
+            
+    ]
 
-def get_data(filters, period_list, partner_doctype):
-	sales_field = frappe.scrub(partner_doctype)
-	sales_users_data = get_parents_data(filters, partner_doctype)
+    return columns
 
-	if not sales_users_data:
-		return
-	sales_users = []
-	sales_user_wise_item_groups = {}
+def get_data(filters, partner_doctype):
+    sales_field = frappe.scrub(partner_doctype)
+    sales_users_data = get_parents_data(filters, partner_doctype)
+    if not sales_users_data:
+        return
+    sales_users = []
 
-	for d in sales_users_data:
-		if d.parent not in sales_users:
-			sales_users.append(d.parent)
+    for d in sales_users_data:
+        if d.parent not in sales_users:
+            sales_users.append(d.parent)
 
-		sales_user_wise_item_groups.setdefault(d.parent, [])
-		if d.item_group:
-			sales_user_wise_item_groups[d.parent].append(d.item_group)
+    date_field = "posting_date"
 
-	date_field = "posting_date"
+    actual_data = get_actual_data(filters, sales_users, date_field, sales_field)
 
-	actual_data = get_actual_data(filters, sales_users, date_field, sales_field)
-
-	return prepare_data(
-		filters,
-		sales_users_data,
-		sales_user_wise_item_groups,
-		actual_data,
-		date_field,
-		period_list,
-		sales_field,
-	)
-
-
-def get_columns(filters, period_list, partner_doctype):
-	fieldtype, options = "Currency", "currency"
-
-	columns = [
-		{
-			"fieldname": frappe.scrub(partner_doctype),
-			"label": _(partner_doctype),
-			"fieldtype": "Link",
-			"options": partner_doctype,
-			"width": 150,
-		},
-		{
-			"fieldname": "item_group",
-			"label": _("Item Group"),
-			"fieldtype": "Link",
-			"options": "Item Group",
-			"width": 150,
-		},
-	]
-
-	for period in period_list:
-		target_key = f"target_{period.key}"
-		variance_key = f"variance_{period.key}"
-
-		columns.extend(
-			[
-				{
-					"fieldname": target_key,
-					"label": _("Target ({})").format(period.label),
-					"fieldtype": fieldtype,
-					"options": options,
-					"width": 150,
-				},
-				{
-					"fieldname": period.key,
-					"label": _("Achieved ({})").format(period.label),
-					"fieldtype": fieldtype,
-					"options": options,
-					"width": 150,
-				},
-				{
-					"fieldname": variance_key,
-					"label": _("Variance ({})").format(period.label),
-					"fieldtype": fieldtype,
-					"options": options,
-					"width": 150,
-				},
-			]
-		)
-
-	columns.extend(
-		[
-			{
-				"fieldname": "total_target",
-				"label": _("Total Target"),
-				"fieldtype": fieldtype,
-				"options": options,
-				"width": 150,
-			},
-			{
-				"fieldname": "total_achieved",
-				"label": _("Total Achieved"),
-				"fieldtype": fieldtype,
-				"options": options,
-				"width": 150,
-			},
-			{
-				"fieldname": "total_variance",
-				"label": _("Total Variance"),
-				"fieldtype": fieldtype,
-				"options": options,
-				"width": 150,
-			},
-		]
-	)
-
-	return columns
+    return prepare_data(
+        filters,
+        sales_users_data,
+        actual_data,
+        sales_field,
+    )
 
 
 def prepare_data(
-	filters,
-	sales_users_data,
-	sales_user_wise_item_groups,
-	actual_data,
-	date_field,
-	period_list,
-	sales_field,
+    filters,
+    sales_users_data,
+    actual_data,
+    sales_field,
 ):
-	rows = {}
+    rows = {}
 
-	target_qty_amt_field = "target_amount"
-	qty_or_amount_field = "base_net_amount"
+    target_qty_amt_field = "target_amount"
+    qty_or_amount_field = "base_net_amount"
 
-	item_group_parent_child_map = get_item_group_parent_child_map()
+    item_group_parent_child_map = get_item_group_parent_child_map()
 
-	for d in sales_users_data:
-		key = (d.parent, d.item_group)
-		dist_data = get_periodwise_distribution_data(d.distribution_id, period_list, filters.get("period"))
+    for d in sales_users_data:
 
-		if key not in rows:
-			rows.setdefault(key, {"total_target": 0, "total_achieved": 0, "total_variance": 0})
+        if d.parent not in rows:
+            rows.setdefault(d.parent, {"total_target": 0, "total_achieved": 0, "per_achieved": 0, "total_variance": 0, "per_day": 0, "salary": 0, "per_salary": 0})
 
-		details = rows[key]
-		for period in period_list:
-			p_key = period.key
-			if p_key not in details:
-				details[p_key] = 0
+        details = rows[d.parent]
+        
+        target_percentage = get_target_percentage(filters, d.distribution_id)
+        target_amount = d.get(target_qty_amt_field)*target_percentage/100
+        details["total_target"] += target_amount
+        for r in actual_data:
+            if (
+                r.get(sales_field) == d.parent
+                and (
+                    r.item_group == d.item_group
+                    or r.item_group in item_group_parent_child_map.get(d.item_group, [])
+                )
+            ):
+                details["total_achieved"] += r.get(qty_or_amount_field, 0)
 
-			target_key = f"target_{p_key}"
-			variance_key = f"variance_{p_key}"
-			details[target_key] = (d.get(target_qty_amt_field) * dist_data.get(p_key)) / 100
-			details[variance_key] = 0
-			details["total_target"] += details[target_key]
+        details["total_variance"] = details.get("total_achieved") - details.get("total_target")
+        details["per_achieved"] = details.get("total_achieved") / details.get("total_target") * 100
 
-			for r in actual_data:
-				if (
-					r.get(sales_field) == d.parent
-					and period.from_date <= r.get(date_field)
-					and r.get(date_field) <= period.to_date
-					and (
-						not sales_user_wise_item_groups.get(d.parent)
-						or r.item_group == d.item_group
-						or r.item_group in item_group_parent_child_map.get(d.item_group, [])
-					)
-				):
-					details[p_key] += r.get(qty_or_amount_field, 0)
-					details[variance_key] = details.get(p_key) - details.get(target_key)
-				else:
-					details[variance_key] = details.get(p_key) - details.get(target_key)
-
-			details["total_achieved"] += details.get(p_key)
-			details["total_variance"] = details.get("total_achieved") - details.get("total_target")
-
-	return rows
+    return rows
 
 
 def get_item_group_parent_child_map():
-	"""
-	Returns a dict of all item group parents and leaf children associated with them.
-	"""
+    """
+    Returns a dict of all item group parents and leaf children associated with them.
+    """
 
-	item_groups = frappe.get_all(
-		"Item Group", fields=["name", "parent_item_group"], order_by="lft desc, rgt desc"
-	)
-	item_group_parent_child_map = {}
+    item_groups = frappe.get_all(
+        "Item Group", fields=["name", "parent_item_group"], order_by="lft desc, rgt desc"
+    )
+    item_group_parent_child_map = {}
 
-	for item_group in item_groups:
-		children = item_group_parent_child_map.get(item_group.name, [])
-		if not children:
-			children = [item_group.name]
-		item_group_parent_child_map.setdefault(item_group.parent_item_group, []).extend(children)
+    for item_group in item_groups:
+        children = item_group_parent_child_map.get(item_group.name, [])
+        if not children:
+            children = [item_group.name]
+        item_group_parent_child_map.setdefault(item_group.parent_item_group, []).extend(children)
 
-	return item_group_parent_child_map
+    return item_group_parent_child_map
 
 
 def get_actual_data(filters, sales_users_or_territory_data, date_field, sales_field):
-	fiscal_year = get_fiscal_year(fiscal_year=filters.get("fiscal_year"), as_dict=1)
+    start_date, end_date = get_start_date_end_date(filters)
+    parent_doc = frappe.qb.DocType("Sales Invoice")
+    child_doc = frappe.qb.DocType("Sales Invoice Item")
 
-	parent_doc = frappe.qb.DocType("Sales Invoice")
-	child_doc = frappe.qb.DocType("Sales Invoice Item")
+    query = frappe.qb.from_(parent_doc).inner_join(child_doc).on(child_doc.parent == parent_doc.name)
 
-	query = frappe.qb.from_(parent_doc).inner_join(child_doc).on(child_doc.parent == parent_doc.name)
+    if sales_field == "sales_person":
+        sales_team = frappe.qb.DocType("Sales Team")
+        net_amount = sales_team.custom_contribution_to_margin
+        sales_field_col = sales_team[sales_field]
 
-	if sales_field == "sales_person":
-		sales_team = frappe.qb.DocType("Sales Team")
-		net_amount = sales_team.custom_contribution_to_margin
-		sales_field_col = sales_team[sales_field]
+        query = query.inner_join(sales_team).on(sales_team.parent == parent_doc.name)
+    else:
+        net_amount = parent_doc.custom_net_amount_eligible_for_commission
+        sales_field_col = parent_doc[sales_field]
 
-		query = query.inner_join(sales_team).on(sales_team.parent == parent_doc.name)
-	else:
-		net_amount = parent_doc.custom_net_amount_eligible_for_commission
-		sales_field_col = parent_doc[sales_field]
+    query = query.select(
+        child_doc.item_group,
+        parent_doc[date_field],
+        (net_amount).as_("base_net_amount"),
+        sales_field_col,
+    ).where(
+        #For testing
+        (parent_doc.docstatus == 0)
+        & (parent_doc[date_field].between(start_date, end_date))
+        & (sales_field_col.isin(sales_users_or_territory_data))
+    )
 
-	query = query.select(
-		child_doc.item_group,
-		parent_doc[date_field],
-		(net_amount).as_("base_net_amount"),
-		sales_field_col,
-	).where(
-		#For testing
-		(parent_doc.docstatus == 0)
-		& (parent_doc[date_field].between(fiscal_year.year_start_date, fiscal_year.year_end_date))
-		& (sales_field_col.isin(sales_users_or_territory_data))
-	)
-
-	return query.run(as_dict=True)
+    return query.run(as_dict=True)
 
 
 def get_parents_data(filters, partner_doctype):
-	filters_dict = {"parenttype": partner_doctype}
-	if filters.get(frappe.scrub(partner_doctype)):
-		filters_dict["parent"] = filters.get(frappe.scrub(partner_doctype))
-		
-	target_qty_amt_field = "target_amount"
+    filters_dict = {"parenttype": partner_doctype}
+    if filters.get(frappe.scrub(partner_doctype)):
+        filters_dict["parent"] = filters.get(frappe.scrub(partner_doctype))
+        
+    target_qty_amt_field = "target_amount"
+    
+    start_date, end_date = get_start_date_end_date(filters)
 
-	if filters.get("fiscal_year"):
-		filters_dict["fiscal_year"] = filters.get("fiscal_year")
+    fiscal_year =  get_fiscal_year(start_date)
+    filters_dict["fiscal_year"] = fiscal_year[0]
 
-	return frappe.get_all(
-		"Target Detail",
-		filters=filters_dict,
-		fields=["parent", "item_group", target_qty_amt_field, "fiscal_year", "distribution_id"],
-	)
+    return frappe.get_all(
+        "Target Detail",
+        filters=filters_dict,
+        fields=["parent", "item_group", target_qty_amt_field, "fiscal_year", "distribution_id"],
+    )
+
+def get_start_date_end_date(filters):
+    if filters.get("period") == "Fiscal Year":
+        fiscal_year = get_fiscal_year(fiscal_year=filters.get("fiscal_year"), as_dict=1)
+        return fiscal_year.year_start_date, fiscal_year.year_end_date
+    elif filters.get("period") == "MTD":
+        end_date = filters.get("date")
+        start_date = get_first_day(end_date)
+        return start_date, end_date
+    elif filters.get("period") == "Month":
+        start_date = getdate("{1}-{0}-01".format(filters.get("month"), filters.get("year")))
+        end_date = get_last_day(start_date)
+        return start_date, end_date
+
+def get_target_percentage(filters, distribution_id):
+    doc = frappe.get_doc("Monthly Distribution", distribution_id)
+    if filters.get("period") == "Fiscal Year":
+        return 100.00
+    elif filters.get("period") == "MTD":
+        date = getdate(filters.get("date"))
+        month = date.strftime("%B").title()
+        start_date = get_first_day(date)
+        end_date = get_last_day(start_date)
+        total_days_of_month = date_diff(end_date, start_date)
+        mtd_days = date_diff(date, start_date)
+        total_percent = 0
+        for d in doc.percentages:
+            if d.month == month:
+                total_percent += d.percentage_allocation
+        return total_percent*mtd_days/total_days_of_month
+    elif filters.get("period") == "Month":
+        start_date = getdate("{1}-{0}-01".format(filters.get("month"), filters.get("year")))
+        month = start_date.strftime("%B").title()
+        for d in doc.percentages:
+            if d.month == month:
+                return d.percentage_allocation
+
+    
+
+@frappe.whitelist()
+def get_attendance_years() -> str:
+    """Returns all the years for which attendance records exist"""
+    sals_invoice = frappe.qb.DocType("Sales Invoice")
+    year_list = (
+        frappe.qb.from_(sals_invoice)
+        .select(Extract("year", sals_invoice.posting_date).as_("year"))
+        .distinct()
+    ).run(as_dict=True)
+
+    if year_list:
+        year_list.sort(key=lambda d: d.year, reverse=True)
+    else:
+        year_list = [frappe._dict({"year": getdate().year})]
+
+    return "\n".join(cstr(entry.year) for entry in year_list)
